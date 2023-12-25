617ee926d09bba5008cc919440f8cadb0d14b619
SolveigRebnord <91594496+SolveigRebnord@users.noreply.github.com>
2023-12-25 11:00:36 +0100

    Initial Commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter, Lusitana } from 'next/font/google';
+ 
+export const inter = Inter({ subsets: ['latin'] });
+// gir __className_e66fe9
+
+export const lusitana = Lusitana({ 
+    subsets: ['latin'],
+    weight: ['400', '700'] 
+});
